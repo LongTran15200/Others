75fad026d1db01f7d22158cdc7a17696b7e6b10d
Long Tran <128632373+LongTran15200@users.noreply.github.com>
2024-08-23 01:23:29 -0500

    Create drawTextColors.asm

---
+++ drawTextColors.asm
@@ -0,0 +1,31 @@
+;DrawTextColors
+;Long Tran
+;Computer Organization
+;COSC 2325 2801
+;October 11, 2022
+;Write a program that displays the same string in four different colors, using a loop. 
+;Call the SetTextColor procedure from following link library. 
+;Any Colors may be chosen, but you may find it easiest to change the foreground color.
+
+
+include Irvine32.inc
+
+.data
+str1 byte "This line is displayed in color",0
+
+.code
+main proc
+
+mov eax, black + (white * 16) ; black on white backgrouund
+mov ecx,4 ; loop counter
+
+L1: call SetTextColor
+mov edx,OFFSET str1
+call WriteString
+call Crlf
+add eax,2 ; add 2 to foreground color
+loop L1
+
+exit
+main endp
+end main
